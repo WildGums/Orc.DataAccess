54038756cd69c6e829f77abf526c4ca1f59dedce
Vladimir <work.lvg@gmail.com>
2019-05-31 19:02:25 +0400

    little fixes

--- src/Orc.DataAccess/Excel/Models/ExcelSource.cs
+++ src/Orc.DataAccess/Excel/Models/ExcelSource.cs
@@ -8,6 +8,8 @@ namespace Orc.DataAccess.Excel
 {
     public class ExcelSource : DataSourceBase
     {
+        private string _topLeftCell = "A1";
+
         #region Constructors
         public ExcelSource()
             : this(string.Empty)
@@ -23,7 +25,21 @@ namespace Orc.DataAccess.Excel
         #region Properties
         public string FilePath { get; set; }
         public string Worksheet { get; set; }
-        public string TopLeftCell { get; set; } = "A1";
+
+        public string TopLeftCell
+        {
+            get => _topLeftCell;
+            set
+            {
+                if (Equals(_topLeftCell, value))
+                {
+                    return;
+                }
+
+                _topLeftCell = value;
+                RaisePropertyChanged(nameof(TopLeftCell));
+            }
+        }
         #endregion
     }
 }
